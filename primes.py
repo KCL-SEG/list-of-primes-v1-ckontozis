f744f796cbd9adb72d5f40e5848bf7f30b55dc29
Constantinos Kontozis <constantinos.kontozis@kcl.ac.uk>
2023-10-01 00:32:31 +0100

    Add functionality to list the first n primes

--- primes.py
+++ primes.py
@@ -1,6 +1,12 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if all(num % prime != 0 for prime in list):
+            list.append(num)
+        num += 1
     return list
